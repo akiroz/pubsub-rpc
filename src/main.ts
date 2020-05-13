c0eb3cffd5f342f658588320ddf8053a5cdce05c
akiroz <git@akiroz.life>
2020-05-13 17:50:40 +0800

    support whildcard subscriptions

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,17 @@
 import * as MsgPack from "@msgpack/msgpack";
-import { EventEmitter } from "events";
+import { EventEmitter2 } from "eventemitter2";
 import DedupCache from "./dedupCache";
 import { encodeBase64URL, generateCallId } from "./util";
 
-export type RPCRequest = { id: Uint8Array; params: any; };
+export type RPCRequest = { id: Uint8Array; params: any };
 
 export type RPCResponse = { result: any } & { error: { message: string; data?: any } };
 
 export type PubSubClient = {
     publish(topic: string, payload: Uint8Array): Promise<void>;
-    subscribe(topic: string, handler: (payload: Uint8Array) => void): Promise<void>;
+    subscribe(topic: string, handler: (payload: Uint8Array, topic: string) => void): Promise<void>;
     unsubscribe(topic: string): Promise<void>;
-}
+};
 
 const idDedup = new DedupCache(100);
 
@@ -20,41 +20,50 @@ export async function register<T extends { [k: string]: any }, R extends { [k: s
     topic: string,
     handler: (arg: T) => Promise<R>
 ) {
-    await client.subscribe(topic, async (msg) => {
+    await client.subscribe(topic, async (msg, msgTopic) => {
         try {
             const { id, params } = MsgPack.decode(msg) as RPCRequest;
-            if(!id) throw Error("Missing id in RPC call");
+            if (!id) throw Error("Missing id in RPC call");
             const strId = encodeBase64URL(id);
-            if(idDedup.has(strId)) throw Error("Duplicate call request");
+            if (idDedup.has(strId)) throw Error("Duplicate call request");
             idDedup.put(strId);
             const response = await handler(params)
                 .then((result) => ({ result }))
                 .catch((error) => ({ error }));
-            await client.publish(`${topic}/${strId}`, MsgPack.encode(response));
-        } catch(err) {
+            await client.publish(`${msgTopic}/${strId}`, MsgPack.encode(response));
+        } catch (err) {
             console.warn("RPC processing error", err);
         }
     });
 }
 
-export async function call(client: PubSubClient, topic: string, params: any = {}, opt = { timeout: 10000 }): Promise<any> {
+export async function call(
+    client: PubSubClient,
+    topic: string,
+    params: any = {},
+    opt = { timeout: 10000 }
+): Promise<any> {
     const id = generateCallId(16);
     const strId = encodeBase64URL(id);
-    const ee = new EventEmitter();
+    const ee = new EventEmitter2();
     const responseTopic = `${topic}/${strId}`;
     await client.subscribe(responseTopic, (msg) => ee.emit("resp", msg));
     client.publish(topic, MsgPack.encode({ id, params }));
-    const msg = await Promise.race([
-        new Promise((rsov, rjct) => ee.once("resp", (payload) => {
-            client.unsubscribe(responseTopic);
-            rsov(payload);
-        })),
-        new Promise((rsov, rjct) => setTimeout(() => {
-            client.unsubscribe(responseTopic);
-            rjct({ message: "timeout", data: { topic, params, opt } });
-        }, opt.timeout)),
-    ]) as Uint8Array;
+    const msg = (await Promise.race([
+        new Promise((rsov, rjct) =>
+            ee.once("resp", (payload) => {
+                client.unsubscribe(responseTopic);
+                rsov(payload);
+            })
+        ),
+        new Promise((rsov, rjct) =>
+            setTimeout(() => {
+                client.unsubscribe(responseTopic);
+                rjct({ message: "timeout", data: { topic, params, opt } });
+            }, opt.timeout)
+        ),
+    ])) as Uint8Array;
     const { result, error } = MsgPack.decode(msg) as RPCResponse;
-    if(error) throw error;
+    if (error) throw error;
     return result;
 }
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,28 +1,22 @@
 import { strict as assert } from "assert";
 import * as RPC from "../src/main";
-import { EventEmitter } from "events";
+import { EventEmitter2 } from "eventemitter2";
 
 let pubSub: RPC.PubSubClient;
 
 describe("RPC", () => {
     beforeEach(() => {
-        const ee = new EventEmitter();
+        const ee = new EventEmitter2({ wildcard: true, delimiter: "/" });
         pubSub = {
-            publish(topic, payload) {
-                return new Promise(r => {
-                    setTimeout(() => (ee.emit(topic, payload), r()), 0);
-                });
+            async publish(topic, payload) {
+                await ee.emitAsync(topic, payload, topic);
             },
-            subscribe(topic, handler) {
-                return new Promise(r => {
-                    setTimeout(() => (ee.on(topic, handler), r()), 0);
-                });
+            async subscribe(topic, handler) {
+                ee.on(topic, handler);
             },
-            unsubscribe(topic) {
-                return new Promise(r => {
-                    setTimeout(() => (ee.removeAllListeners(topic), r()), 0);
-                });
-            }
+            async unsubscribe(topic) {
+                ee.removeAllListeners(topic);
+            },
         };
     });
 
@@ -32,20 +26,29 @@ describe("RPC", () => {
     });
 
     it("can send/recv params/result", async () => {
-        await RPC.register(pubSub, "topic/foo", async ({ a, b }) => ({ c: a+b }));
-        const { c } = await RPC.call(pubSub, "topic/foo", { a:2, b:3 });
+        await RPC.register(pubSub, "topic/foo", async ({ a, b }) => ({ c: a + b }));
+        const { c } = await RPC.call(pubSub, "topic/foo", { a: 2, b: 3 });
         assert.equal(c, 5);
+    });
+
+    it("can register with wildcards", async () => {
+        await RPC.register(pubSub, "*/foo", async (param) => param);
+        const { test1 } = await RPC.call(pubSub, "1/foo", { test1: "test1" });
+        const { test2 } = await RPC.call(pubSub, "2/foo", { test2: "test2" });
+        assert.equal(test1, "test1");
+        assert.equal(test2, "test2");
     });
 
     it("should timeout if call took too long", async () => {
         await RPC.register(pubSub, "topic/foo", () => new Promise(() => {}));
         try {
             await RPC.call(pubSub, "topic/foo", {}, { timeout: 50 });
-        } catch(err) {
+        } catch (err) {
             assert(err.message, "timeout");
         }
     });
-
 });
 
-after(() => { if(typeof window === 'undefined') process.exit(0) });
+after(() => {
+    if (typeof window === "undefined") process.exit(0);
+});
